3316a38d8e9b752372eb2e5659d37e17e33387b9
Alvin Uchenna <43749581+Veri5ied@users.noreply.github.com>
2022-06-21 12:18:56 +0100

    success screens

---
+++ src/pages/reset-password/OtpScreen.jsx
@@ -0,0 +1,55 @@
+import "./style.scss";
+import ChefLogo from "../../assets/svgs/chefsvg.svg";
+import PasswordSuccess from "./PasswordSuccess";
+import { useEffect, useState } from "react";
+import AuthCode from "react-auth-code-input";
+
+const OtpScreen = () => {
+  const [otpCode, setOtpCode] = useState(false);
+  const [istrue, setIsTrue] = useState(false);
+
+  const handleChange = (res) => {
+    setOtpCode(res);
+    console.log(res);
+  };
+
+  const sendOtp = () => {
+    console.log("Sent!!!");
+    setIsTrue(true);
+  };
+
+  useEffect(() => {
+    if (otpCode?.length === 4) {
+      sendOtp();
+    }
+  }, [otpCode?.length]);
+  return (
+    <>
+      {istrue ? (
+        <PasswordSuccess />
+      ) : (
+        <div className="sign_container">
+          <div className="sign_body  container">
+            <div className="sign_header">
+              <img src={ChefLogo} alt="logo" />
+            </div>
+            <div className="sign_otp_form">
+              <p>We sent you an OTP code. Please enter for verification</p>
+            </div>
+            <div className="otp_field">
+              <AuthCode
+                onChange={handleChange}
+                allowedCharacters="numeric"
+                length={4}
+                containerClassName="otp_container"
+                className="otp_container"
+              />
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default OtpScreen;
--- src/pages/signup-page/SignUp.jsx
+++ src/pages/signup-page/SignUp.jsx
@@ -10,112 +10,128 @@ import FacebookSVG from "../../assets/svgs/facebook_svg.svg";
 import AuxiliaryButton from "../../components/buttons/auxiliary-button/AuxiliaryButton";
 import GoogleSVG from "../../assets/svgs/google_svg.svg";
 import { SIGNIN_PAGE } from "../../routes";
+import OtpScreen from "./OtpScreen";
 
 const SignUp = () => {
   const [form] = Form.useForm();
   const formRef = useRef();
   const [loading, setLoading] = useState(false);
+  const [istrue, setIsTrue] = useState(false);
   const onFinish = (values) => {
     setLoading(true);
     console.log(values);
     setLoading(false);
+    setIsTrue(true);
   };
   return (
-    <div className="sign_container">
-      <div className="sign_body  container">
-        <div className="sign_header">
-          <img src={ChefLogo} alt="logo" />
-          <h1>Registration</h1>
-        </div>
-        <div className="sign_form">
-          <Form onFinish={onFinish} form={form} ref={formRef}>
-            <Form.Item
-              name="fullName"
-              rules={[
-                {
-                  required: true,
-                  message: "Full Name is required!",
-                },
-              ]}
-            >
-              <Input
-                placeholder="Your Name"
-                className="form_input"
-                type="text"
-              />
-            </Form.Item>
-            <Form.Item
-              name="email"
-              rules={[
-                {
-                  required: true,
-                  message: "Email is required!",
-                },
-                {
-                  type: "email",
-                  message: "The email is not a valid email",
-                },
-              ]}
-            >
-              <Input placeholder="Email" type="email" className="form_input" />
-            </Form.Item>
-            <Form.Item
-              name="phone"
-              rules={[
-                {
-                  required: true,
-                  message: "Phone Number is required!",
-                },
-              ]}
-            >
-              <Input placeholder="Mobile" type="tel" className="form_input" />
-            </Form.Item>
-            <Form.Item
-              name="password"
-              hasFeedback
-              rules={[
-                {
-                  required: true,
-                  message: "Please input your password",
-                },
-              ]}
-            >
-              <Input
-                placeholder="Password"
-                type="password"
-                className="form_input"
-              />
-            </Form.Item>
-            {!loading && (
-              <PrimaryButton buttonText="Registration" htmlType="submit" />
-            )}
-            {loading && <LoadingButton buttonText="Loading..." />}
-          </Form>
-          <div className="form_extra_auth">
-            <div className="or_sign">
-              <hr />
-              <p>
-                <span>or</span>
-              </p>
-              <hr />
+    <>
+      {istrue && <OtpScreen />}
+      {!istrue && (
+        <div className="sign_container">
+          <div className="sign_body  container">
+            <div className="sign_header">
+              <img src={ChefLogo} alt="logo" />
+              <h1>Registration</h1>
             </div>
-            <div className="auth_btns">
-              <SecondaryButton
-                buttonText="Sign with facebook account"
-                imgUrl={FacebookSVG}
-              />
-              <AuxiliaryButton
-                buttonText="Sign with google account"
-                imgUrl={GoogleSVG}
-              />
+            <div className="sign_form">
+              <Form onFinish={onFinish} form={form} ref={formRef}>
+                <Form.Item
+                  name="fullName"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Full Name is required!",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Your Name"
+                    className="form_input"
+                    type="text"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="email"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Email is required!",
+                    },
+                    {
+                      type: "email",
+                      message: "The email is not a valid email",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Email"
+                    type="email"
+                    className="form_input"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="phone"
+                  rules={[
+                    {
+                      required: true,
+                      message: "Phone Number is required!",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Mobile"
+                    type="tel"
+                    className="form_input"
+                  />
+                </Form.Item>
+                <Form.Item
+                  name="password"
+                  hasFeedback
+                  rules={[
+                    {
+                      required: true,
+                      message: "Please input your password",
+                    },
+                  ]}
+                >
+                  <Input
+                    placeholder="Password"
+                    type="password"
+                    className="form_input"
+                  />
+                </Form.Item>
+                {!loading && (
+                  <PrimaryButton buttonText="Registration" htmlType="submit" />
+                )}
+                {loading && <LoadingButton buttonText="Loading..." />}
+              </Form>
+              <div className="form_extra_auth">
+                <div className="or_sign">
+                  <hr />
+                  <p>
+                    <span>or</span>
+                  </p>
+                  <hr />
+                </div>
+                <div className="auth_btns">
+                  <SecondaryButton
+                    buttonText="Sign with facebook account"
+                    imgUrl={FacebookSVG}
+                  />
+                  <AuxiliaryButton
+                    buttonText="Sign with google account"
+                    imgUrl={GoogleSVG}
+                  />
+                </div>
+                <p>
+                  Already have an account? <Link to={SIGNIN_PAGE}>Log in!</Link>
+                </p>
+              </div>
             </div>
-            <p>
-              Already have an account? <Link to={SIGNIN_PAGE}>Log in!</Link>
-            </p>
           </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
