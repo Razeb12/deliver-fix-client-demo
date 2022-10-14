f3784b8bd5e07b5f5d08d56878bfa38388ac6db5
Veri5ied <alvinuchenna14@gmail.com>
2022-10-14 10:10:26 +0100

    get all products

--- src/pages/order-pages/BurgerPage.jsx
+++ src/pages/order-pages/BurgerPage.jsx
@@ -1,12 +1,49 @@
 import OrderCard from "./order-cards/OrderCard";
+import { Pagination } from "antd";
+import { useContext, useEffect } from "react";
 import "./style.scss";
 import { BurgerMenu } from "./data";
+import GeneralContext from "../../context/general-context/GeneralContext";
 
 const BurgerPage = () => {
-  const logToConsole = (e) => {
-    console.log(`${e} added to cart`);
+  const { products, getAllProducts } = useContext(GeneralContext);
+
+  useEffect(() => {
+    getAllProducts(1, 6);
+  }, []);
+
+  const handlePagination = (page) => {
+    getAllProducts(page, 6);
   };
+
+  console.log(products);
+
   return (
+    <div className="food_order">
+      <div className="order-food__list">
+        {products.data &&
+          products.data.map((item, index) => (
+            <OrderCard
+              key={index}
+              image={item.image}
+              name={item.name}
+              price={item.price}
+              description={item.description}
+            />
+          ))}
+      </div>
+      <div className="order-food__pagination">
+        <Pagination
+          defaultCurrent={1}
+          total={products[1]}
+          onChange={handlePagination}
+        />
+      </div>
+    </div>
+  );
+};
+
+/*  return (
     <div className="food_order">
       {BurgerMenu.map((item, index) => (
         <OrderCard
@@ -23,8 +60,14 @@ const BurgerPage = () => {
           onClick={() => logToConsole(item.name)}
         />
       ))}
+      <div
+        className="food_pagination"
+        style={{ marginTop: "10px", textAlign: "center" }}
+      >
+        <Pagination defaultCurrent={1} total={50} />
+      </div>
     </div>
   );
-};
+}; */
 
 export default BurgerPage;
