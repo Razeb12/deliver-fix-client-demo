fd2261bebf1c7ea3ada3485943392a682a9f0ce6
Veri5ied <alvinuchenna14@gmail.com>
2022-10-14 10:14:46 +0100

    setup

--- src/context/general-context/GeneralReducer.js
+++ src/context/general-context/GeneralReducer.js
@@ -1,4 +1,4 @@
-import { GET_ALL_PRODUCTS } from "../types";
+import { GET_ALL_PRODUCTS, GET_PRODUCT_BY_CATEGORY } from "../types";
 
 const GeneralReducer = (prevState, { type, payload }) => {
   switch (type) {
@@ -6,6 +6,11 @@ const GeneralReducer = (prevState, { type, payload }) => {
       return {
         ...prevState,
         products: payload,
+      };
+    case GET_PRODUCT_BY_CATEGORY:
+      return {
+        ...prevState,
+        categories: payload,
       };
     default:
       return prevState;
--- src/context/types.js
+++ src/context/types.js
@@ -5,6 +5,7 @@ export const FORGOT_PASSWORD = "FORGOT_PASSWORD";
 export const RESET_PASSWORD = "RESET_PASSWORD";
 export const RESEND_OTP = "RESEND_OTP";
 export const GET_ALL_PRODUCTS = "GET_ALL_PRODUCTS";
+export const GET_PRODUCT_BY_CATEGORY = "GET_PRODUCT_BY_CATEGORY";
 export const ADD_TO_CART = "ADD_TO_CART";
 export const GET_CART = "GET_CART";
 export const REMOVE_FROM_CART = "REMOVE_FROM_CART";
